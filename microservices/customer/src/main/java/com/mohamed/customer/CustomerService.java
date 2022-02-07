ff76b66b023dd94449e1c6919a8d24d331c611e2
Abrar Ahamed <mohamed.abrar@philips.com>
2022-02-07 10:46:39 +0530

    added fraud service

--- microservices/customer/src/main/java/com/mohamed/customer/CustomerService.java
+++ microservices/customer/src/main/java/com/mohamed/customer/CustomerService.java
@@ -2,7 +2,7 @@ package com.mohamed.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
@@ -11,13 +11,22 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    RestTemplate template;
+
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = new Customer();
         customer.setFirstname( request.getFirstName());
         customer.setEmail(request.getEmail());
         customer.setLastName(request.getEmail());
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = template.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}" , FraudCheckResponse.class , customer.getId()
+        );
+
+        //System.out.println(fraudCheckResponse.isFraud());
 
     }
 }
---
+++ microservices/customer/src/main/java/com/mohamed/customer/FraudCheckResponse.java
@@ -0,0 +1,13 @@
+package com.mohamed.customer;
+
+public class FraudCheckResponse {
+    private boolean isFraud;
+
+    public boolean isFraud() {
+        return isFraud;
+    }
+
+    public void setFraud(boolean fraud) {
+        isFraud = fraud;
+    }
+}
